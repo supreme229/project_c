1228e6de825c088981911a46e5acab030d215f7d
supreme229 <58195100+supreme229@users.noreply.github.com>
2020-02-11 02:22:52 +0100

    Skonczenie gry karcianej

--- card.c
+++ card.c
@@ -8,17 +8,17 @@ int main()
   int which_option;
   int *card_option = &which_option;
 
-  int plr_pts = 0,enm_pts = 0,boost_plr = 0,boost_enm = 0,ace_nblock = 0;
+  int plr_pts = 0,enm_pts = 0,boost_plr = 0,boost_enm = 0;
 
-  int card_iterator = 11, *player_points = &plr_pts, *enemy_points = &enm_pts, *start_boost_plr = &boost_plr, *start_boost_enemy = &boost_enm, *ace_not_blocked = &ace_nblock;
-  main_war(1,power1_left,power2_left,power3_left,card_option,card_iterator,player_points,enemy_points,start_boost_plr,start_boost_enemy,ace_not_blocked);
+  int card_iterator = 11, *player_points = &plr_pts, *enemy_points = &enm_pts, *start_boost_plr = &boost_plr, *start_boost_enm = &boost_enm;
+  main_war(1,power1_left,power2_left,power3_left,card_option,card_iterator,player_points,enemy_points,start_boost_plr,start_boost_enm);
 
   while(card_iterator != -1)
   {
-    main_war(2, power1_left, power2_left, power3_left, card_option, card_iterator, player_points, enemy_points, start_boost_plr, start_boost_enemy,ace_not_blocked);
+    main_war(2, power1_left, power2_left, power3_left, card_option, card_iterator, player_points, enemy_points, start_boost_plr, start_boost_enm);
     card_iterator--;
 /*
-    if(*start_boost_enemy == 1)
+    if(*start_boost_enm == 1)
     *enemy_points = 2;
     else *enemy_points = 0;
 
--- cards.h
+++ cards.h
@@ -8,6 +8,7 @@
 #include <stdio.h>
 
 #include "structures.h"
+#include "texts.h"
 
 //zamiana wartosci
 void swap(char *a, char *b)
@@ -34,7 +35,7 @@ void what_powers(char id,int iter,int yMax)
             refresh();
             break;
         case '4':
-            mvprintw(yMax+7+iter,0,"Blokada jopka, traci wlasciowsc bicia asa u przeciwnika w rundzie. Numer mocy: %d\n",iter+1);
+            mvprintw(yMax+7+iter,0,"Dodanie 2 pkt twojej karcie. Numer mocy: %d\n",iter+1);
             refresh();
             break;
         case '5':
@@ -102,7 +103,7 @@ void give_cards_and_powers(int yMax)
      for(int i=0;i<6;i++)
      {
         mvprintw(yMax+3,place,"%c ",player_cards.card_id[i]);
-        mvprintw(yMax+5,place,"%c ",enemy_cards.card_id[i]);
+        //mvprintw(yMax+5,place,"%c ",enemy_cards.card_id[i]);
         pause1(1.0);
         refresh();
         place+=2;
@@ -111,14 +112,14 @@ void give_cards_and_powers(int yMax)
      for(int i=6;i<12;i++)
      {
        mvprintw(yMax+4,place-12,"%c ",player_cards.card_id[i]);
-       mvprintw(yMax+6,place-12,"%c ",enemy_cards.card_id[i]);
+      // mvprintw(yMax+6,place-12,"%c ",enemy_cards.card_id[i]);
        pause1(1.0);
        refresh();
        place+=2;
      }
 
      attron(A_BOLD);
-    // mvprintw(yMax+6,0,"Twoje moce:");
+     mvprintw(yMax+6,0,"Twoje moce:");
      attroff(A_BOLD);
 
      int iter = 0;
@@ -126,12 +127,13 @@ void give_cards_and_powers(int yMax)
      for(int i=0;i<3;i++)
      {
       what_powers(player_cards.powers[i],iter,yMax);
+      //what_powers(enemy_cards.powers[i],iter+3,yMax);
       pause1(1.0);
       iter++;
-     }
+    }
 }
 
-void plr_power_points(char id,int *player_points, int *enemy_points, int *start_boost_plr, int *ace_not_blocked, int *power1_left, int *power2_left, int *power3_left)
+void plr_power_points(char id,int *player_points, int *enemy_points, int *start_boost_plr, int *power1_left, int *power2_left, int *power3_left)
 {
   switch(id)
   {
@@ -149,7 +151,7 @@ void plr_power_points(char id,int *player_points, int *enemy_points, int *start_
     break;
 
     case '4':
-    *ace_not_blocked = 1;
+    *player_points += 2;
     break;
 
     case '5':
@@ -162,115 +164,297 @@ void plr_power_points(char id,int *player_points, int *enemy_points, int *start_
   }
 }
 
-void plr_card_points(char id,int *player_points)
+void enm_power_points(char id,int *player_points, int *enemy_points, int *start_boost_enm, int *enemy_powers_used)
+{
+  switch(id)
+  {
+    case '1':
+    *enemy_points *= 2;
+    break;
+
+    case '2':
+    *player_points -= 2;
+    break;
+
+    case '3':
+    *enemy_points = 100;
+    *start_boost_enm = 1;
+    break;
+
+    case '4':
+    *enemy_points += 2;
+    break;
+
+    case '5':
+    *enemy_points += 3;
+    break;
+
+    case '6':
+    *player_points /= 2;
+    break;
+  }
+  *enemy_powers_used = *enemy_powers_used + 1;
+}
+
+void plr_card_points(char id,int *player_points, int *enemy_points)
 {
   switch(id)
   {
    case 'T':
-      *player_points = 5;
+      *player_points += 5;
       break;
 
     case '9':
-      *player_points = 3;
+      *player_points += 3;
       break;
 
     case 'Q':
-      *player_points  = 6;
+      *player_points  += 6;
       break;
 
     case 'K':
-      *player_points = 8;
+      *player_points += 8;
       break;
 
     case 'A':
-      *player_points = 12;
+      *player_points += 12;
       break;
 
     case 'J':
-      *player_points = 0;
+      *player_points += 0;
+      *enemy_points -= 5;
       break;
  }
 }
 
-void enm_card_points(char id,int *enemy_points)
+void enm_card_points(char id,int *enemy_points, int *player_points)
 {
   switch(id)
   {
    case 'T':
-      *enemy_points = 5;
+      *enemy_points += 5;
       break;
 
     case '9':
-      *enemy_points = 3;
+      *enemy_points += 3;
       break;
 
     case 'Q':
-      *enemy_points  = 6;
+      *enemy_points += 6;
       break;
 
     case 'K':
-      *enemy_points = 8;
+      *enemy_points += 8;
       break;
 
     case 'A':
-      *enemy_points = 12;
+      *enemy_points += 12;
       break;
 
     case 'J':
-      *enemy_points = 0;
+      *enemy_points += 0;
+      *player_points -= 5;
       break;
  }
 }
 
-void what_chosen(int *chosen, WINDOW *menu, int card_iterator, int yMax, int *player_points, int *enemy_points, int *start_boost_plr, int *ace_not_blocked, int *power1_left, int *power2_left, int *power3_left)
+void what_chosen_option(int *enemy_powers_used, int *rounds_player, int *rounds_enemy,int *chosen, WINDOW *menu, int card_iterator, int yMax, int *player_points, int *enemy_points, int *start_boost_plr, int *power1_left, int *power2_left, int *power3_left, int *start_boost_enm)
 {
   switch(*chosen)
     {
     case 1:
       //nastepna karta po prostu
-      mvwprintw(menu,yMax-9,1,"Twoja karta to: %c",player_cards.card_id[card_iterator]);
-      mvwprintw(menu,yMax-8,1,"Nr uzytej mocy: Nie uzyto mocy!");
-      plr_card_points(player_cards.card_id[card_iterator],player_points);
-      enm_card_points(enemy_cards.card_id[card_iterator],enemy_points);
-      mvwprintw(menu,yMax-7,1,"Ilosc twoich pkt: %d",*player_points);
-      mvwprintw(menu,yMax-6,1,"Ilosc pkt rywala: %d",*enemy_points);
+      wattron(menu,A_UNDERLINE);
+      wattron(menu,A_BOLD);
+      mvwprintw(menu,yMax-14,1,"GRACZ:");
+      wattroff(menu,A_UNDERLINE);
+      wattroff(menu,A_BOLD);
+      pause1(1.0);
+      wrefresh(menu);
+      mvwprintw(menu,yMax-13,1,"Rzucona karta: %c",player_cards.card_id[card_iterator]);
+      pause1(1.0);
+      wrefresh(menu);
+      mvwprintw(menu,yMax-12,1,"Uzyto mocy: NIE");
+      pause1(1.0);
+      wrefresh(menu);
+      wattron(menu,A_UNDERLINE);
+      wattron(menu,A_BOLD);
+      mvwprintw(menu,yMax-10,1,"PRZECIWNIK:");
+      wattroff(menu,A_UNDERLINE);
+      wattroff(menu,A_BOLD);
+      plr_card_points(player_cards.card_id[card_iterator],player_points,enemy_points);
+      enm_card_points(enemy_cards.card_id[card_iterator],enemy_points,player_points);
+      if(*rounds_player - *rounds_enemy >= 2 && *enemy_powers_used != 3)
+        {
+          enm_power_points(enemy_cards.powers[*enemy_powers_used - 1], player_points,enemy_points,start_boost_enm,enemy_powers_used);
+          pause1(1.0);
+          wrefresh(menu);
+          mvwprintw(menu,yMax-8,1,"Uzyto mocy: TAK",enemy_cards.powers[*enemy_powers_used -1]);
+        }
+        else
+        {
+          pause1(1.0);
+          wrefresh(menu);
+          mvwprintw(menu,yMax-8,1,"Uzyto mocy: NIE",enemy_cards.powers[*enemy_powers_used -1]);
+        }
 
+      pause1(1.0);
+      wrefresh(menu);
+      mvwprintw(menu,yMax-9,1,"Rzucona karta: %c",enemy_cards.card_id[card_iterator]);
+      pause1(1.0);
+      wrefresh(menu);
+      pause1(1.0);
+      wrefresh(menu);
+      mvwprintw(menu,yMax-5,1,"Ilosc twoich pkt: %d",*player_points);
+      mvwprintw(menu,yMax-4,1,"Ilosc pkt rywala: %d",*enemy_points);
       break;
     case 2:
       //uzycie mocy1 i nastepna karta
-      mvwprintw(menu,yMax-9,1,"Twoja karta to: %c",player_cards.card_id[card_iterator]);
-      mvwprintw(menu,yMax-8,1,"Nr uzytej mocy: 1");
-      plr_card_points(player_cards.card_id[card_iterator],player_points);
-      enm_card_points(enemy_cards.card_id[card_iterator],enemy_points);
-      plr_power_points(player_cards.powers[0],player_points,enemy_points,ace_not_blocked,start_boost_plr,power1_left, power2_left, power3_left);
-      mvwprintw(menu,yMax-7,1,"Ilosc twoich pkt: %d",*player_points);
-      mvwprintw(menu,yMax-6,1,"Ilosc pkt rywala: %d",*enemy_points);
+      wattron(menu,A_UNDERLINE);
+      wattron(menu,A_BOLD);
+      mvwprintw(menu,yMax-14,1,"GRACZ:");
+      wattroff(menu,A_UNDERLINE);
+      wattroff(menu,A_BOLD);
+      pause1(1.0);
+      wrefresh(menu);
+      mvwprintw(menu,yMax-13,1,"Rzucona karta: %c",player_cards.card_id[card_iterator]);
+      pause1(1.0);
+      wrefresh(menu);
+      mvwprintw(menu,yMax-12,1,"Uzyto mocy: TAK");
+      wattron(menu,A_UNDERLINE);
+      wattron(menu,A_BOLD);
+      pause1(1.0);
+      wrefresh(menu);
+      mvwprintw(menu,yMax-10,1,"PRZECIWNIK:");
+      wattroff(menu,A_UNDERLINE);
+      wattroff(menu,A_BOLD);
+      plr_card_points(player_cards.card_id[card_iterator],player_points,enemy_points);
+      enm_card_points(enemy_cards.card_id[card_iterator],enemy_points,player_points);
+      plr_power_points(player_cards.powers[0],player_points,enemy_points,start_boost_plr,power1_left, power2_left, power3_left);
+      if(*rounds_player - *rounds_enemy >= 2 && *enemy_powers_used != 3)
+      {
+        enm_power_points(enemy_cards.powers[*enemy_powers_used - 1], player_points,enemy_points,start_boost_enm,enemy_powers_used);
+        pause1(1.0);
+        wrefresh(menu);
+        mvwprintw(menu,yMax-8,1,"Uzyto mocy: TAK",enemy_cards.powers[*enemy_powers_used -1]);
+      }
+      else
+      {
+        pause1(1.0);
+        wrefresh(menu);
+        mvwprintw(menu,yMax-8,1,"Uzyto mocy: NIE",enemy_cards.powers[*enemy_powers_used -1]);
+      }
+
+      pause1(1.0);
+      wrefresh(menu);
+      mvwprintw(menu,yMax-9,1,"Rzucona karta: %c",enemy_cards.card_id[card_iterator]);
+      pause1(1.0);
+      wrefresh(menu);
+      pause1(1.0);
+      wrefresh(menu);
+      mvwprintw(menu,yMax-5,1,"Ilosc twoich pkt: %d",*player_points);
+      mvwprintw(menu,yMax-4,1,"Ilosc pkt rywala: %d",*enemy_points);
       move(yMax+7,0);
       clrtoeol();
       *power1_left = *power1_left - 1;
       break;
     case 3:
       //uzcie mocy2 i nastepna karta
-      mvwprintw(menu,yMax-9,1,"Twoja karta to: %c",player_cards.card_id[card_iterator]);
-      mvwprintw(menu,yMax-8,1,"Nr uzytej mocy: 2");
-      plr_card_points(player_cards.card_id[card_iterator],player_points);
-      enm_card_points(enemy_cards.card_id[card_iterator],enemy_points);
-      plr_power_points(player_cards.powers[1],player_points,enemy_points,ace_not_blocked,start_boost_plr,power1_left, power2_left, power3_left);
-      mvwprintw(menu,yMax-7,1,"Ilosc twoich pkt: %d",*player_points);
-      mvwprintw(menu,yMax-6,1,"Ilosc pkt rywala: %d",*enemy_points);
+      wattron(menu,A_UNDERLINE);
+      wattron(menu,A_BOLD);
+      mvwprintw(menu,yMax-14,1,"GRACZ:");
+      wattroff(menu,A_UNDERLINE);
+      wattroff(menu,A_BOLD);
+      pause1(1.0);
+      wrefresh(menu);
+      mvwprintw(menu,yMax-13,1,"Rzucona karta: %c",player_cards.card_id[card_iterator]);
+      pause1(1.0);
+      wrefresh(menu);
+      mvwprintw(menu,yMax-12,1,"Uzyto mocy: TAK");
+      wattron(menu,A_UNDERLINE);
+      wattron(menu,A_BOLD);
+      pause1(1.0);
+      wrefresh(menu);
+      mvwprintw(menu,yMax-10,1,"PRZECIWNIK:");
+      wattroff(menu,A_UNDERLINE);
+      wattroff(menu,A_BOLD);
+      plr_card_points(player_cards.card_id[card_iterator],player_points,enemy_points);
+      enm_card_points(enemy_cards.card_id[card_iterator],enemy_points,player_points);
+      plr_power_points(player_cards.powers[1],player_points,enemy_points,start_boost_plr,power1_left, power2_left, power3_left);
+      if(*rounds_player - *rounds_enemy >= 2 && *enemy_powers_used != 3)
+      {
+        enm_power_points(enemy_cards.powers[*enemy_powers_used - 1], player_points,enemy_points,start_boost_enm,enemy_powers_used);
+        pause1(1.0);
+        wrefresh(menu);
+        mvwprintw(menu,yMax-8,1,"Uzyto mocy: TAK",enemy_cards.powers[*enemy_powers_used -1]);
+      }
+      else
+      {
+        pause1(1.0);
+        wrefresh(menu);
+        mvwprintw(menu,yMax-8,1,"Uzyto mocy: NIE",enemy_cards.powers[*enemy_powers_used -1]);
+      }
+      pause1(1.0);
+      wrefresh(menu);
+      mvwprintw(menu,yMax-9,1,"Rzucona karta: %c",enemy_cards.card_id[card_iterator]);
+      pause1(1.0);
+      wrefresh(menu);
+      pause1(1.0);
+      wrefresh(menu);
+      mvwprintw(menu,yMax-5,1,"Ilosc twoich pkt: %d",*player_points);
+      mvwprintw(menu,yMax-4,1,"Ilosc pkt rywala: %d",*enemy_points);
       move(yMax+8,0);
       clrtoeol();
       *power2_left = *power2_left - 1;
       break;
     case 4:
       //uzycie mocy3 i nastepna karta
-      mvwprintw(menu,yMax-9,1,"Twoja karta to: %c",player_cards.card_id[card_iterator]);
-      mvwprintw(menu,yMax-8,1,"Nr uzytej mocy: 3");
-      plr_card_points(player_cards.card_id[card_iterator],player_points);
-      enm_card_points(enemy_cards.card_id[card_iterator],enemy_points);
-      plr_power_points(player_cards.powers[2],player_points,enemy_points,ace_not_blocked,start_boost_plr,power1_left, power2_left, power3_left);
-      mvwprintw(menu,yMax-7,1,"Ilosc twoich pkt: %d",*player_points);
-      mvwprintw(menu,yMax-6,1,"Ilosc pkt rywala: %d",*enemy_points);
+      wattron(menu,A_UNDERLINE);
+      wattron(menu,A_BOLD);
+      mvwprintw(menu,yMax-14,1,"GRACZ:");
+      wattroff(menu,A_UNDERLINE);
+      wattroff(menu,A_BOLD);
+      pause1(1.0);
+      wrefresh(menu);
+      mvwprintw(menu,yMax-13,1,"Rzucona karta: %c",player_cards.card_id[card_iterator]);
+      pause1(1.0);
+      wrefresh(menu);
+      mvwprintw(menu,yMax-12,1,"Uzyto mocy: TAK");
+      pause1(1.0);
+      wrefresh(menu);
+      wattron(menu,A_UNDERLINE);
+      wattron(menu,A_BOLD);
+      mvwprintw(menu,yMax-10,1,"PRZECIWNIK:");
+      wattroff(menu,A_UNDERLINE);
+      wattroff(menu,A_BOLD);
+      plr_card_points(player_cards.card_id[card_iterator],player_points,enemy_points);
+      enm_card_points(enemy_cards.card_id[card_iterator],enemy_points,player_points);
+      plr_power_points(player_cards.powers[2],player_points,enemy_points,start_boost_plr,power1_left, power2_left, power3_left);
+      if(*rounds_player - *rounds_enemy >= 2 && *enemy_powers_used != 3)
+      {
+        enm_power_points(enemy_cards.powers[*enemy_powers_used - 1], player_points,enemy_points,start_boost_enm,enemy_powers_used);
+        pause1(1.0);
+        wrefresh(menu);
+        mvwprintw(menu,yMax-8,1,"Uzyto mocy: TAK",enemy_cards.powers[*enemy_powers_used -1]);
+      }
+      else
+      {
+        pause1(1.0);
+        wrefresh(menu);
+        mvwprintw(menu,yMax-8,1,"Uzyto mocy: NIE",enemy_cards.powers[*enemy_powers_used -1]);
+      }
+
+      pause1(1.0);
+      wrefresh(menu);
+      mvwprintw(menu,yMax-9,1,"Rzucona karta: %c",enemy_cards.card_id[card_iterator]);
+
+      pause1(1.0);
+      wrefresh(menu);
+      pause1(1.0);
+      wrefresh(menu);
+
+      mvwprintw(menu,yMax-5,1,"Ilosc twoich pkt: %d",*player_points);
+      mvwprintw(menu,yMax-4,1,"Ilosc pkt rywala: %d",*enemy_points);
       move(yMax+9,0);
       clrtoeol();
       *power3_left = *power3_left - 1;
@@ -279,14 +463,40 @@ void what_chosen(int *chosen, WINDOW *menu, int card_iterator, int yMax, int *pl
     wrefresh(menu);
 }
 
-void main_war(int intro,int *power1_left, int *power2_left, int *power3_left, int *chosen,int card_iterator, int *player_points, int *enemy_points, int *start_boost_plr, int *start_boost_enemy,int *ace_not_blocked)
+void write_cards(int yMax)
+{
+
+int place = 0;
+
+  for(int i=0;i<6;i++)
+  {
+     mvprintw(yMax+3,place,"%c ",player_cards.card_id[i]);
+     //mvprintw(yMax+5,place,"%c ",enemy_cards.card_id[i]);
+     //pause1(1.0);
+     refresh();
+     place+=2;
+  }
+
+  for(int i=6;i<12;i++)
+  {
+    mvprintw(yMax+4,place-12,"%c ",player_cards.card_id[i]);
+   // mvprintw(yMax+6,place-12,"%c ",enemy_cards.card_id[i]);
+    //pause1(1.0);
+    refresh();
+    place+=2;
+  }
+
+}
+
+void main_war(int intro, int *enemy_powers_used, int *power1_left, int *power2_left, int *power3_left, int *chosen,int card_iterator, int *player_points, int *enemy_points, int *start_boost_plr, int *start_boost_enm, int *rounds_player, int *rounds_enemy)
 {
   initscr();
   noecho();
 
   int yMax, xMax, yBeg, xBeg;
   //DEKLARACJA OKNA
-  WINDOW * menu = newwin(15,35,1,0);
+  WINDOW * menu = newwin(20,35,1,0);
+
   box(menu,0,0);
 
     if(intro == 1)
@@ -325,20 +535,27 @@ void main_war(int intro,int *power1_left, int *power2_left, int *power3_left, in
     refresh();
 
     attron(A_BOLD);
-    mvprintw(yBeg,32,"Przypomnienie oznaczen kart:");
+    mvprintw(yBeg,37,"Przypomnienie oznaczen kart:");
     attroff(A_BOLD);
-    mvprintw(yBeg + 1, 37,"A - as");
-    mvprintw(yBeg + 2, 37,"K - krol");
-    mvprintw(yBeg + 3, 37,"Q - dama");
-    mvprintw(yBeg + 4, 37,"T - dziesiatka");
-    mvprintw(yBeg + 5, 37,"9 - dziewiatka");
-    mvprintw(yBeg + 6, 37,"J - jopek");
+    mvprintw(yBeg + 1, 37,"A - as (12 pkt)");
+    mvprintw(yBeg + 2, 37,"K - krol (8 pkt)");
+    mvprintw(yBeg + 3, 37,"Q - dama (6 pkt)");
+    mvprintw(yBeg + 4, 37,"T - dziesiatka (5 pkt)");
+    mvprintw(yBeg + 5, 37,"9 - dziewiatka (3 pkt)");
+    mvprintw(yBeg + 6, 37,"J - jopek (0 pkt ale przeciwnik -5 pkt)");
 
     refresh();
 
   }
 else
 {
+  attron(A_BOLD);
+  mvprintw(yMax+2,xBeg+15,"Nastepna karta: ",player_cards.card_id[card_iterator]);
+  attroff(A_BOLD);
+  mvprintw(yMax+3,xBeg+15,"%c",player_cards.card_id[card_iterator]);
+  write_cards(yMax);
+
+
   keypad(menu,true);
 
   int show = 0;
@@ -400,13 +617,44 @@ while(true)
             }
       }
   }
-  what_chosen(chosen,menu,card_iterator,yMax,player_points,enemy_points,start_boost_plr,ace_not_blocked,power1_left,power2_left,power3_left);
+  what_chosen_option(enemy_powers_used,rounds_player,rounds_enemy,chosen,menu,card_iterator,yMax,player_points,enemy_points,start_boost_plr,power1_left,power2_left,power3_left,start_boost_enm);
   pause1(1.0);
   if(*power1_left == 0 && *power2_left == 0 && *power3_left == 0)
     {
       move(yMax+6,0);
       clrtoeol();
     }
+
+  if(*player_points > *enemy_points)
+  {
+    *rounds_player = *rounds_player + 1;
+    wattron(menu,A_BLINK);
+    mvwprintw(menu,yMax-2,1,"Wygrywasz ta runde!");
+    wattroff(menu,A_BLINK);
+  }
+  else
+    if(*player_points < *enemy_points)
+    {
+      *rounds_enemy = *rounds_enemy + 1;
+      wattron(menu,A_BLINK);
+      mvwprintw(menu,yMax-2,1,"Przegrywasz ta runde...");
+      wattroff(menu,A_BLINK);
+    }
+    else
+     {
+      wattron(menu,A_BLINK);
+      mvwprintw(menu,yMax-2,1,"Remis!");
+      wattroff(menu,A_BLINK);
+    }
+    wrefresh(menu);
+
+    attron(A_BOLD);
+    mvprintw(yBeg+9,41,"Ilosc rund wygranych:");
+    attroff(A_BOLD);
+    mvprintw(yBeg+10,37,"Lambert: %d      Przeciwnik: %d ",*rounds_player,*rounds_enemy);
+    refresh();
+
+
   getch();
 }
 
